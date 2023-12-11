53b733b1bbc0c8ca5354956ff2cc8d11b59195a4
Matthew Walton <matthew@mathw.me.uk>
2023-12-11 15:52:54 +0000

    Day 5 part 2 is giving 0 for some reason

--- src/day5.rs
+++ src/day5.rs
@@ -1,22 +1,34 @@
 use crate::daylib::{DayResult, Parts, Result};
-use std::{collections::HashSet, str::FromStr};
+use std::{collections::HashSet, ops::Range, str::FromStr};
 
 pub fn day5(input: &str) -> DayResult {
     let r = part1(input)?;
+    let r2 = part2(input)?;
 
     Ok(Parts {
         part1: Box::new(format!("The smallest location is {}", r)),
-        part2: Box::new(format!("not done yet")),
+        part2: Box::new(format!("The smallest expanded location is {}", r2)),
     })
 }
+
 #[derive(Clone, Debug)]
 struct WeirdMap {
     ranges: Vec<MappedRange>,
 }
 
 impl WeirdMap {
-    fn map(&self, source: u64) -> HashSet<u64> {
-        let set: HashSet<u64> = self.ranges.iter().filter_map(|r| r.map(source)).collect();
+    fn new(ranges: Vec<MappedRange>) -> Self {
+        Self { ranges }
+    }
+
+    fn map(&self, source: Range<u64>) -> HashSet<Range<u64>> {
+        let set: HashSet<Range<u64>> = self
+            .ranges
+            .iter()
+            .filter_map(|r| r.map(source.clone()))
+            .flatten()
+            .collect();
+
         if set.is_empty() {
             HashSet::from([source])
         } else {
@@ -27,31 +39,29 @@ impl WeirdMap {
 
 #[test]
 fn test_weirdmap_mapping() {
-    let map = WeirdMap {
-        ranges: vec![
-            MappedRange {
-                source_start: 4,
-                source_end: 8,
-                dest_start: 10,
-            },
-            MappedRange {
-                source_start: 10,
-                source_end: 15,
-                dest_start: 20,
-            },
-            MappedRange {
-                source_start: 12,
-                source_end: 15,
-                dest_start: 30,
-            },
-        ],
-    };
+    let map = WeirdMap::new(vec![
+        MappedRange {
+            source_start: 4,
+            source_end: 8,
+            dest_start: 10,
+        },
+        MappedRange {
+            source_start: 10,
+            source_end: 15,
+            dest_start: 20,
+        },
+        MappedRange {
+            source_start: 12,
+            source_end: 15,
+            dest_start: 30,
+        },
+    ]);
 
-    assert_eq!(map.map(3), HashSet::from([3]));
-    assert_eq!(map.map(4), HashSet::from([10]));
-    assert_eq!(map.map(10), HashSet::from([20]));
-    assert_eq!(map.map(15), HashSet::from([15]));
-    assert_eq!(map.map(12), HashSet::from([30, 22]));
+    assert_eq!(map.map(3..4), HashSet::from([3..4]));
+    assert_eq!(map.map(4..5), HashSet::from([10..11]));
+    assert_eq!(map.map(10..11), HashSet::from([20..21]));
+    assert_eq!(map.map(15..16), HashSet::from([15..16]));
+    assert_eq!(map.map(12..13), HashSet::from([30..31, 22..23]));
 }
 
 #[derive(PartialEq, Eq, Debug, Clone)]
@@ -62,11 +72,59 @@ struct MappedRange {
 }
 
 impl MappedRange {
-    fn map(&self, source: u64) -> Option<u64> {
-        if source >= self.source_start && source < self.source_end {
-            Some(self.dest_start + (source - self.source_start))
-        } else {
+    fn map_value(&self, v: u64) -> Option<u64> {
+        Some(match self.source_start.cmp(&self.dest_start) {
+            std::cmp::Ordering::Less => v + (self.dest_start - self.source_start),
+            std::cmp::Ordering::Equal => v,
+            std::cmp::Ordering::Greater => v - (self.source_start - self.dest_start),
+        })
+    }
+
+    fn map(&self, source: Range<u64>) -> Option<HashSet<Range<u64>>> {
+        let mut did_map = false;
+        let mut ranges = HashSet::new();
+
+        // any unmapped range which exists before us
+        if source.start < self.source_start {
+            let end = u64::min(source.end, self.source_start);
+            ranges.insert(source.start..end);
+        }
+
+        // any range where we overlap
+        let start = u64::max(source.start, self.source_start);
+        let end = u64::min(source.end, self.source_end);
+        if start < end {
+            did_map = true;
+            // now we need to map it into the destination range
+            match self.source_start.cmp(&self.dest_start) {
+                std::cmp::Ordering::Less => {
+                    let offset = self.dest_start - self.source_start;
+                    let start = start + offset;
+                    let end = end + offset;
+                    ranges.insert(start..end);
+                }
+                std::cmp::Ordering::Equal => {
+                    ranges.insert(start..end);
+                }
+                std::cmp::Ordering::Greater => {
+                    let offset = self.source_start - self.dest_start;
+                    let start = start - offset;
+                    let end = end - offset;
+                    ranges.insert(start..end);
+                }
+            }
+        }
+
+        // any unmapped range which exists after us
+        if source.end > self.source_end {
+            let start = u64::max(self.source_end, source.start);
+            ranges.insert(start..source.end);
+        }
+
+        if ranges.is_empty() || !did_map {
             None
+        } else {
+            Some(ranges)
         }
     }
 }
@@ -78,19 +136,10 @@ fn test_range_mapping() {
         source_end: 8,
         dest_start: 10,
     };
-    assert_eq!(range.map(3), None);
-    assert_eq!(range.map(4), Some(10));
-    assert_eq!(range.map(7), Some(13));
-    assert_eq!(range.map(8), None);
-}
-
-fn parse_weirdmap(input: &str) -> Result<WeirdMap> {
-    let ranges = input
-        .lines()
-        .map(parse_range)
-        .collect::<Result<Vec<MappedRange>>>()?;
-
-    Ok(WeirdMap { ranges })
+    assert_eq!(range.map(3..4), None);
+    assert_eq!(range.map(4..5), Some(HashSet::from([10..11])));
+    assert_eq!(range.map(7..8), Some(HashSet::from([13..14])));
+    assert_eq!(range.map(8..9), None);
 }
 
 fn parse_range(input: &str) -> Result<MappedRange> {
@@ -134,8 +183,7 @@ struct WeirdMaps {
 }
 
 impl WeirdMaps {
-    fn locations_for_seed(&self, seed: u64) -> HashSet<u64> {
-        println!("Finding location for {}...", seed);
+    fn locations_for_seed(&self, seed: Range<u64>) -> HashSet<Range<u64>> {
         self.seed_to_soil
             .map(seed)
             .into_iter()
@@ -149,14 +197,14 @@ impl WeirdMaps {
     }
 }
 
-fn parse_input(input: &str) -> Result<(HashSet<u64>, WeirdMaps)> {
+fn parse_input(input: &str) -> Result<(Vec<u64>, WeirdMaps)> {
     let mut lines = input.lines();
     let first_line = lines.next().unwrap();
     let seeds = first_line
         .trim_start_matches("seeds: ")
         .split_ascii_whitespace()
         .map(u64::from_str)
-        .collect::<std::result::Result<HashSet<u64>, _>>()?;
+        .collect::<std::result::Result<Vec<u64>, _>>()?;
     lines.next();
     lines.next();
 
@@ -168,9 +216,7 @@ fn parse_input(input: &str) -> Result<(HashSet<u64>, WeirdMaps)> {
             continue;
         }
         if line.ends_with(" map:") {
-            current_maps.push(WeirdMap {
-                ranges: current_ranges,
-            });
+            current_maps.push(WeirdMap::new(current_ranges));
             current_ranges = Vec::new();
             continue;
         }
@@ -178,9 +224,7 @@ fn parse_input(input: &str) -> Result<(HashSet<u64>, WeirdMaps)> {
         current_ranges.push(range);
     }
 
-    current_maps.push(WeirdMap {
-        ranges: current_ranges,
-    });
+    current_maps.push(WeirdMap::new(current_ranges));
 
     if current_maps.len() != 7 {
         return Err(format!(
@@ -206,11 +250,28 @@ fn parse_input(input: &str) -> Result<(HashSet<u64>, WeirdMaps)> {
 
 fn part1(input: &str) -> Result<u64> {
     let (seeds, weirdmaps) = parse_input(input)?;
-    seeds
+    let ranges = seeds
+        .into_iter()
+        .flat_map(|s| weirdmaps.locations_for_seed(s..s + 1));
+    let first_range = ranges.min_by_key(|r| r.start).map(|r| r.start);
+
+    Ok(first_range.ok_or("No location mapped".to_owned())?)
+}
+
+fn part2(input: &str) -> Result<u64> {
+    let (seeds, weirdmaps) = parse_input(input)?;
+    let ranges: Vec<Range<u64>> = seeds
+        .chunks_exact(2)
+        .map(|chunk| chunk[0]..chunk[0] + chunk[1])
+        .collect();
+    println!("{:?}", ranges);
+    let range = ranges
         .into_iter()
         .flat_map(|s| weirdmaps.locations_for_seed(s))
-        .min()
-        .ok_or("No locations found".to_owned().into())
+        .min_by_key(|r| r.start);
+    Ok(range
+        .expect("There should at least be a range, right?")
+        .start)
 }
 
 #[cfg(test)]
@@ -257,7 +318,13 @@ fn test_part1() {
 #[test]
 fn test_first_seed() {
     let (_seeds, weirdmaps) = parse_input(TEST_INPUT).unwrap();
-    let l = weirdmaps.locations_for_seed(79);
+    let l = weirdmaps.locations_for_seed(79..80);
     assert_eq!(l.len(), 1);
-    assert_eq!(l.into_iter().next().unwrap(), 82);
+    assert_eq!(l.into_iter().next().unwrap(), 82..83);
+}
+
+#[test]
+fn test_part2() {
+    let r = part2(TEST_INPUT).unwrap();
+    assert_eq!(r, 46);
 }
